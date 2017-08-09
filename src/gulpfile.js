2197cc516673e0611af318b6b51af1251fb25c3e
Mads Kristensen <post@madskristensen.net>
2017-08-09 11:25:09 -0700

    Cleaned up build script

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -4,7 +4,6 @@ var gulp = require("gulp"),
     bom = require("gulp-bom"),
     rename = require("gulp-rename"),
     shell = require("gulp-shell"),
-    zip = require("gulp-zip"),
     rimraf = require("rimraf");
 
 var manifest = JSON.parse(require("fs").readFileSync('./app/manifest.json'));
@@ -22,12 +21,6 @@ gulp.task("clean:dist", function (cb) {
 
 
 // Package
-gulp.task("webextension:package", function (cb) {
-    return gulp.src("app/**/*")
-        .pipe(zip(manifest.name + "-" + manifest.version + ".zip"))
-        .pipe(gulp.dest("dist"));
-});
-
 gulp.task("edge:package", shell.task([
     "manifoldjs -l debug -p edgeextension package temp\\edgeextension\\manifest\\"
 ]));
