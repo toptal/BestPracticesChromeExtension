34adc1204f22ba4e904397360190629beda1bad5
Mads Kristensen <post@madskristensen.net>
2017-08-09 10:43:16 -0700

    Fixed build script

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
-Remove-Item src\dist\*
+Remove-Item src\dist\*.zip
 
+# We zip in powershell instead of in Gulp due to issues with gulp-zip not maintaining a BOM
 Compress-Archive -Path src\app\* -DestinationPath "src\dist\Web Developer Checklist.zip"
